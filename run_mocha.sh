f97adfe05644a9ac78a7293a06ae455f65efdb8d
Aaron Aichlmayr <waterfoul@gmail.com>
2016-07-15 13:21:05 -0500

    fixing the coverage report

--- run_mocha.sh
+++ run_mocha.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 ./node_modules/.bin/istanbul cover \
-    -x *.spec.js \
-    -x *.spec.class.js \
     -i './{,!(node_modules|coverage|webpack|mocks)/**/}/{,!(index|mochaInit|AuthHandler)*}.js' \
-    --include-all-sources \
     ./node_modules/.bin/_mocha \
         -r node_modules/reflect-metadata \
-        -r ./mochaInit './{,!(node_modules)/**/}/*.spec.js'
+        -r ./mochaInit \
+        './{,!(node_modules)/**/}/*.spec.js'
